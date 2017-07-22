f223c614d99ae3033cc451975ccbf19afbd048ff
Tomasz <tomasso.holy@gmail.com>
2017-07-22 16:53:51 +0200

    play icon

--- src/assets/js/generateAudio.js
+++ src/assets/js/generateAudio.js
@@ -1,65 +1,117 @@
 
 var audioVisualizer ={
+  init: function(){
+    audioVisualizer.controlsInit();
+    audioVisualizer.audioAnalyseInit();
 
-  init:  function(){
+    window.addEventListener('resize', resizeCanvas);
+    function resizeCanvas(){
+      canvas.width=window.innerWidth*0.9;
+      audioVisualizer.visualize();
+    }
+    resizeCanvas();
+
+  },
+
+  controlsInit:  function(){
       console.log("generate vis");
       canvas = document.getElementById('visualizer');
       canvasCtx = canvas.getContext("2d");
+      player = document.getElementById('player');
+      play = document.getElementById('play');
+      trackHead = document.getElementById('trackHead');
+      timeline = document.getElementById('trackTimelineWrapper');
+
+      var isTrackheadDragged = false;
       var duration;
 
-      player = document.getElementById('player');
-      player.addEventListener("canplaythrough", function () {
-        duration = player.duration;
-      }, false);
+      player.addEventListener("canplaythrough", getDuration);
+      player.addEventListener("timeupdate", timeUpdate);
 
       //controls
-      play = document.getElementById('play');
       play.addEventListener('click', audioVisualizer.controls.play);
+      trackHead.addEventListener('mousedown', drag);
+      window.addEventListener('mouseup', trackHeadDropped);
 
-      trackHead = document.getElementById('trackHead');
-      player.addEventListener("timeupdate", timeUpdate, false);
+      //get track duration
+      function getDuration(){
+        duration = player.duration;
+      }
 
-      timeline = document.getElementById('trackTimeline');
+      //function to drag trackHead
+      function drag(){
+        isTrackheadDragged = true;
+        trackHead.className = 'grabbed';
+        window.addEventListener('mousemove', dragMouse);
+        player.removeEventListener('timeupdate', timeUpdate);
+
+      }
+
+      //update trackhead and current time when dragging
+      function dragMouse(event){
+        moveTrackHead(event);
+        //live time update of track time in regard of trackhead position
+        player.currentTime = duration * getClickPercent(event);
+      }
+
+      //update current track time when trackhead is dropped
+      function trackHeadDropped(event){
+        if(isTrackheadDragged==true){
+          isTrackheadDragged = false;
+          trackHead.className = 'to-grab';
+          moveTrackHead(event);
+          player.currentTime = duration * getClickPercent(event);
+          console.log('track head dropped');
+          window.removeEventListener('mousemove', dragMouse);
+          player.addEventListener('timeupdate', timeUpdate);
+        }
+      }
+
+      //move trackhead and update current time when clicking on timeline
       timeline.addEventListener('click', function(event){
-        console.log(getClickPercent(event));
         moveTrackHead(event);
         player.currentTime = duration * getClickPercent(event);
-
       })
 
+      //get number [0,1] representing percent of track time
       function getClickPercent(event){
-
           return (event.clientX - timeline.getBoundingClientRect().left)/timeline.clientWidth;
       }
 
+      //move trackhead when clicked
       function moveTrackHead(event){
-        var newMargin = getClickPercent(event);
+        var newMargin = getClickPercent(event)*100;
 
-        trackHead.style.marginLeft = newMargin +'%';
+        // trackHead.style.marginLeft = newMargin +'%';
+
+        if(newMargin<=0){
+          trackHead.style.marginLeft = 0 +'%';
+        }
+        else if(newMargin>0 && newMargin < 100){
+          trackHead.style.marginLeft = newMargin +'%';
+        } else if(newMargin > 100){
+          trackHead.style.marginLeft = 100 +'%';
+        }
+
       }
 
-
+      //move trackhead when track is playing
       function timeUpdate() {
       	var playPercent = 100 * (player.currentTime / duration);
       	trackHead.style.marginLeft = playPercent + "%";
       }
 
-      window.addEventListener('resize', resizeCanvas, false);
+  }, //end of controlsInit
 
-      audioContext = new (window.AudioContext || window.webkitAudioContext)();
-      analyser = audioContext.createAnalyser();
+  audioAnalyseInit: function(){
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    analyser = audioContext.createAnalyser();
+    source = audioContext.createMediaElementSource(player);
+    source.connect(audioContext.destination);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+  }, //end of audioAnalyseInit
 
-      source = audioContext.createMediaElementSource(player);
-      source.connect(audioContext.destination);
-      source.connect(analyser);
-      analyser.connect(audioContext.destination);
-
-      function resizeCanvas(){
-        canvas.width=window.innerWidth*0.9;
-        audioVisualizer.visualize();
-      }
-      resizeCanvas();
-  }, //end of init
 
   visualize: function(){
     //setup
@@ -94,9 +146,11 @@ var audioVisualizer ={
     play: function(){
       if(player.paused){
         player.play();
+        play.style.backgroundImage = 'url(../../assets/svg/play_button_clean.svg)';
       }
       else{
         player.pause();
+        play.style.backgroundImage = 'url(../../assets/svg/pause_button_clean.svg)';
       }
     }, //end of play
 
